7484dc21d80e7aca0253b4b235cba00d7b590647
metalfoxdev <92944520+metalfoxdev@users.noreply.github.com>
2024-04-25 19:41:45 +0100

    added more holy quotes

--- assets/js/pickdesc.js
+++ assets/js/pickdesc.js
@@ -7,6 +7,12 @@ const array = [
     "Definitely not clickbait!",
     "Have you done your homework? Didn't think so.",
     "Yep, you found it.",
+    "Made in England!",
+    "Thatoneham is a holy man",
+    "This site is so cold, it will put your firewall out!",
+    "If two witches had two watches, which witch would be looking at which watch?",
+    "<-- i love arrows -->",
+    "Road work ahead? Uh yea, I sure hope it does."
 ];
 const randomIndex = Math.floor(Math.random() * array.length);
 
