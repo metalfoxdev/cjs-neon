62b5bf925ba30ab4963bbc42dac6895cfa6b3e4d
Thatoneham <davidzubovskis@gmail.com>
2024-05-14 21:02:55 +0100

    updated description!

--- assets/js/pickdesc.js
+++ assets/js/pickdesc.js
@@ -14,7 +14,16 @@ const array = [
     "Road work ahead? Uh yea, I sure hope it does.",
     "Salute Max and Bailey. They both did their TASSSOOOMMMAAAIII and contributed game suggestions. 🫡",
     "If RoBeats is too hard for you, I think that's a skill issue. (I'm taking to you Connar)",
-    "Hi Logan!"
+    "Hi Logan!",
+    "im getting paid with gum, worth it",
+    "You can submit ideas in the q/a page!",
+    "always listening...",
+    "the germans came back on the 15th of may 2024, we survived",
+    "dont try beef noodles from canteen, i tried twice",
+    "spanish is useless, change my mind",
+    "who ever asked for bloxrob can keep dreaming",
+    "if i get pulled out of lesson for this website,🙈 🙉 🙊",
+    "dw olli apps ill revenge you 😡💢"
 ];
 const randomIndex = Math.floor(Math.random() * array.length);
 
